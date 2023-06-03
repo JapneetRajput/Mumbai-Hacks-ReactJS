2762dd3fd8693920b0215a8e968e2b5038208370
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-03 12:43:12 +0530

    Cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,15 +44,7 @@ const Home = () => {
       <Navbar />
       <div className="pt-24 bg-[#0D1117]">
         <Carousel/>
-       
-        {/* <button
-          className="fixed right-0 bottom-0 m-8 text-md shadow-none"
-          onClick={() => navigate("/addPost")}
-        >
-          <AiFillPlusCircle className="w-12 h-12 sm:w-16 sm:h-16" />
-        </button> */}
         <Services/>
-
         <Footer/>
       </div>
     </>
