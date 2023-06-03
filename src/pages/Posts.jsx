6dde7a5e6f27c26b48f71ec3a508c66b77072ba4
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-03 21:22:49 +0530

    Post

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -21,7 +21,7 @@ const Posts = () => {
     });
   };
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState();
 
   useEffect(() => {
     Axios.get(process.env.REACT_APP_API_BASE_URL + "/api/posts/", {
@@ -30,59 +30,16 @@ const Posts = () => {
       },
     })
       .then((res) => {
+        console.log(res.data);
         setPosts(res.data);
       })
       .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
     profileInit();
   }, []);
-  const handleLike = async (postId) => {
-    try {
-      const response = await Axios.post(
-        `${process.env.REACT_APP_API_BASE_URL}/api/posts/${postId}/like`,
-        null,
-        {
-          headers: {
-            authorization: token,
-          },
-        }
-      );
 
-      const updatedPosts = posts.map((post) => {
-        if (post._id === postId) {
-          return { ...post, likes: response.data.likes };
-        }
-        return post;
-      });
-
-      setPosts(updatedPosts);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const handleDislike = async (postId) => {
-    try {
-      const response = await Axios.post(
-        `${process.env.REACT_APP_API_BASE_URL}/api/posts/${postId}/dislike`,
-        null,
-        {
-          headers: {
-            authorization: token,
-          },
-        }
-      );
-
-      const updatedPosts = posts.map((post) => {
-        if (post._id === postId) {
-          return { ...post, dislikes: response.data.dislikes };
-        }
-        return post;
-      });
-
-      setPosts(updatedPosts);
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
     <>
       <Navbar />
@@ -95,12 +52,14 @@ const Posts = () => {
           <AiFillPlusCircle className="w-6 h-6 sm:w-6 sm:h-6 " /> &nbsp; Create Post
           </div>
         </button>
+
       <div className="pt-24 bg-[#010409] flex flex-wrap justify-center ">
           {posts &&
-            posts.map((post) => (
+            posts.map(({ title, city, state, country, category, description, lat, lng, image, _id }) => (
+              <div className="w-86 self-center pt-10 bg-[#0D1117] border-2 border-[#272e38]  rounded-lg shadow   cursor-pointer  mb-5 ml-2 mr-2 hover:-translate-y-1 hover:scale-90 hover:bg-[#212833] duration-300">
               <div
-                key={post._id}
-                className="w-86 self-center pt-10  bg-[#0D1117] border-2 border-[#272e38] hover:border-[#bfc1c4] rounded-lg shadow  hover:bg-[#0d1117] cursor-pointer  mb-5 ml-2 mr-2"
+                style={{ marginLeft: "5%", marginRight: "5%", alignItems: "center" }}
+                className=" rounded overflow-hidden shadow-md self-center "
               >
                 <img src={`${image}`} alt="" className=" self-center rounded-t-lg object-contain w-40 h-40 md:rounded-none md:rounded-l-lg inline" />
                 <div className="px-6 py-4 ">
@@ -133,26 +92,6 @@ const Posts = () => {
                     <BsArrowDownCircle size={25}/>
                   </div>
     </label>
-
-
-                  
-                  {/* <button onClick={() => navigate("/posts")} className=" shadow-none  text-[#d7dfe7] bg-[#1f7e30] font-bold py-2 px-4 mr-4  hover:bg-[#2ea043] rounded-xl w-16 h-10 my-4" > 
-                  <div style={{display: "flex", justifyContent: "center" }}>
-                    <BsArrowUpCircle size={25}/>
-                  </div>
-                  </button>
-
-                    <button
-                      onClick={() => handleDislike(post._id)}
-                      className="shadow-none  text-[#d7dfe7] bg-[#7e1f1f] font-bold py-2 px-4 hover:bg-[#a02e2e] rounded-xl w-16 h-10 my-4"
-                    >
-                      <div
-                        style={{ display: "flex", justifyContent: "center" }}
-                      >
-                        {post.dislikes.length}
-                        <FaThumbsDown size={25} />
-                      </div>
-                    </button> */}
                   </div>
                 </div>
                 
