04e38c335636787f587352181137f479a9a68f26
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-04 09:17:50 +0530

    Conflict Res

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -14,6 +14,7 @@ import {
   getPostsByCities,
   getPostsByCountries,
   getPostsByStates,
+  profileUser,
 } from "../api/service";
 import { AiFillPlusCircle } from "react-icons/ai";
 import toast, { Toaster } from "react-hot-toast";
